8d8c95917ae4bebdf3e5e6c002f1633755f62bf9
Patrick Naughton <patricknaughton01@gmail.com>
2018-07-13 20:14:44 +0200

    Styling changes and options page
    Added bootstrap and chosen.
    Updated the options page.

--- js/options.js
+++ js/options.js
@@ -1,5 +1,28 @@
 console.log("running");
 
+$(function(){
+  $("#jellyCardProgramSelect").chosen();
+  displayConfigMenu();
+  $("#jellyCardProgramSelect").on("change", displayConfigMenu);
+});
+
 function configAnki(){
 
+}
+
+function displayConfigMenu(event){
+  var ankiBlock = document.getElementById("jellyAnkiConfig");
+  var quizletBlock = document.getElementById("jellyQuizletConfig");
+  ankiBlock.style.display = "none";
+  quizletBlock.style.display = "none";
+  switch($("#jellyCardProgramSelect").val()){
+    case "anki":
+      ankiBlock.style.display = "block";
+      break;
+    case "quizlet":
+      quizletBlock.style.display = "block";
+      break;
+    default:
+      console.log("error");
+  }
 }
